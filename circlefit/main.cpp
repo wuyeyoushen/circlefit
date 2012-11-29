7759038839cebd8714870fb3d17117b22f39c9f8
emilian0 <emiliano@mixamo.com>
2012-11-28 16:31:37 -0800

    adding an implementation of lapack libraries and an example that explain how to use them to solve a 2x2 system

--- circlefit/main.cpp
+++ circlefit/main.cpp
@@ -1,12 +1,34 @@
 #include <iostream>
 #include "Point2Dvector.h"
 #include "CircleFitter.h"
-
+#include "../libs/clapack.h"
 using namespace std;
+
+void solve_sample()
+{
+  //solves system
+  // x+y=10
+  // x-y=20
+  //see help @http://www.netlib.org/lapack/lug/node38.html
+  //see help @http://www.netlib.org/clapack/CLAPACK-3.1.1/SRC/sgetrs.c
+  long int info;
+  long int n =2;
+  long int one(1);
+  float a[]={ 1.0f, 1.0f, 1.0f, -1.0f};
+  float b[]={ 10.0f, 20.0f };
+  long int piv[2];
+  sgetrf_(&n,&n,a,&n,piv,&info);
+  cout<<info<<endl;
+  sgetrs_("N",&n,&one,a,&n,piv,b,&n,&info);
+  
+  cout<<info<<endl;
+  cout<<b[0]<<"\t"<<b[1]<<endl;
+}
 
 //main function: accepts as input parameter a matlab file containing the points sampled on the circle
 int main(int argc, char* argv[])
 {
+  solve_sample();
   try
   {
     if(argc<2)
